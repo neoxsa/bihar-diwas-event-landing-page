ff10254bfcad2fbee30540e54a338316c7b3f1d1
NeoXsa <neoxsa8@gmail.com>
2026-03-13 23:59:52 +0530

    style: update code

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,59 +1,26 @@
 import React from 'react'
-import logo from '/images/logo.webp'
-import { NavLink } from 'react-router-dom'
 
 export default function NavBar() {
 
-    // NavLink Class
-    const navLinkClass = ({ isActive }) => `${isActive ? 'text-red-800' : 'text-black'} hover:text-red-700 font-medium transition-all duration-300`
-
     return (
-        <nav className='flex justify-between items-center px-4 mt-2 mx-5 md:py-2 md:px-10 md:mx-10 bg-white/90 backdrop-blur-xl border border-white/10 shadow-xl shadow-black/10 rounded-2xl transition-all duration-300'>
+        <nav className='flex justify-between items-center px-4 py-5 md:px-10 bg-[#701e11] backdrop-blur-xl border-b-5 border-5 border-[#f5e0a6] shadow-xl shadow-black/10 transition-all duration-300 rounded-t-lg'>
             <div>
-                <img src={logo} alt="logo" className='w-30 h-25 xl:w-40 xl:h-22 object-contain' />
+                <h1 className='text-2xl font-semibold text-shadow text-[#f5e0a6] letter-spacing-[0.1em]'>Bihar Diwas 2026</h1>
             </div>
-            <ul className='flex justify-center items-center md:gap-6 gap-2 text-lg'>
-                <li>
-                    <NavLink
-                        to="/"
-                        className={navLinkClass}>
-                        Home
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink
-                        to="/about"
-                        className={navLinkClass}>
-                        About
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink
-                        to="/schedule"
-                        className={navLinkClass}>
-                        Schedule</NavLink></li>
-                <li>
-                    <NavLink
-                        to="/gallery"
-                        className={navLinkClass}>
-                        Gallery
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink
-                        to="/location"
-                        className={navLinkClass}>
-                        Location
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink
-                        to="/contact"
-                        className={navLinkClass}>
-                        Contact
-                    </NavLink>
-                </li>
-            </ul>
+            <div className='flex justify-center items-center md:gap-6 gap-2 text-lg font-semibold text-[#f5e0a6]'>
+                <a
+                    href="#about"
+                    className='hover:text-[#f17b03] font-semibold transition-all duration-300'
+                >About</a>
+                <a
+                    href="#schedule"
+                    className='hover:text-[#f17b03] font-semibold transition-all duration-300'
+                >Schedule</a>
+                <a
+                    href="#gallery"
+                    className='hover:text-[#f17b03] font-semibold transition-all duration-300'
+                >Gallery</a>
+            </div>
         </nav>
     )
 }
