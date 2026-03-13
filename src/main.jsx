45049649b72962a588a22afd03fe48688240f6df
NeoXsa <neoxsa8@gmail.com>
2026-03-14 00:02:39 +0530

    refactor: remove router setup and render App component directly

--- src/main.jsx
+++ src/main.jsx
@@ -2,24 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom'
-import { Home } from './pages/index.js'
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<App />}>
-      <Route index element={<Home />} />
-      {/* <Route path='/about' element={<About />} />
-      <Route path='/schedule' element={<Schedule />} />
-      <Route path='/gallery' element={<Gallery />} />
-      <Route path='/location' element={<Location />} />
-      <Route path='/contact' element={<Contact />} /> */}
-    </Route>
-  )
-)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>,
 )
